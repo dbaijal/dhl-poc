ec71dfb5225bff287ce512bd415ab211147f5dec
Deepti <deepti.baijal@gmail.com>
2025-05-07 08:58:26 +0530

    code

--- blocks/casestudy/casestudy.js
+++ blocks/casestudy/casestudy.js
@@ -1,6 +1,32 @@
 export default async function decorate(block) {
     block.textContent = '';
     block.innerHTML= `
+    <div class="casehero-section">
+  <div class="casehero-container">
+    <div class="casehero-row">
+      
+      <!-- Left: Text Content -->
+      <div class="casehero-text">
+        <p class="casehero-subheading">TECHNOLOGY CASE STUDY</p>
+        <h1 class="casehero-title">DHL SUPPLY CHAIN HELPS BIG TECH WORK FROM ANYWHERE</h1>
+        <div class="casehero-separator"></div>
+        <p class="casehero-intro">
+        Imagine the frustration of your company PC breaking down at home – you can’t get your work done or communicate effectively with colleagues, but you really want to do a great job
+        </p>
+        <p class="casehero-subtext">
+          <small>DHL Supply Chain – Excellence. Simply delivered.</small>
+        </p>
+      </div>
+
+      <!-- Right: Image -->
+      <div class="casehero-image">
+        <picture>
+          <img src="" alt="ENTER ALT TAG TEXT HERE">
+        </picture>
+      </div>
+    </div>
+  </div>
+</div>
     <div class="casestudydesc columns-container">
       <div class="columns-wrapper"><div class="columns columns-2-cols">
           <div>
